2013cf16f58f5944628ff073afd80218e61f61af
taejin Kim <rlaxowls1316@naver.com>
2023-03-28 20:11:29 +0900

    Add antonyms and synonyms component

--- starter-code/js/response_dom.js
+++ starter-code/js/response_dom.js
@@ -74,8 +74,58 @@ function parse(data) {
 
       definition_list.append(definition_list_item);
     }
+
+    const synonyms = document.createElement("div");
+    synonyms.classList.add("synonyms");
+
+    if (data.meanings[i].synonyms.length > 0) {
+      const synonyms_title = document.createElement("h4");
+      synonyms_title.innerText = "Synonyms";
+      synonyms_title.classList.add("synonyms-title");
+
+      const synonyms_contents = document.createElement("div");
+      synonyms_contents.classList.add("synonyms-contents");
+
+      for (let j = 0; j < data.meanings[i].synonyms.length; j++) {
+        const synonyms_item = document.createElement("span");
+        synonyms_item.classList.add("synonyms-item");
+        synonyms_item.innerText = data.meanings[i].synonyms[j];
+
+        synonyms_contents.append(synonyms_item);
+      }
+
+      synonyms.append(synonyms_title);
+      synonyms.append(synonyms_contents);
+    }
+
+    const antonyms = document.createElement("div");
+    synonyms.classList.add("antonyms");
+
+    if (data.meanings[i].antonyms.length > 0) {
+      const antonyms_title = document.createElement("h4");
+      antonyms_title.innerText = "Antonyms";
+      antonyms_title.classList.add("antonyms-title");
+
+      const antonyms_contents = document.createElement("div");
+      antonyms_contents.classList.add("antonyms-contents");
+
+      for (let j = 0; j < data.meanings[i].antonyms.length; j++) {
+        const antonyms_item = document.createElement("span");
+        antonyms_item.classList.add("antonyms-item");
+        antonyms_item.innerText = data.meanings[i].antonyms[j];
+
+        antonyms_contents.append(antonyms_item);
+      }
+
+      antonyms.append(antonyms_title);
+      antonyms.append(antonyms_contents);
+    }
+
     definition.append(definition_list);
     meanings.append(definition);
+    meanings.append(synonyms);
+    meanings.append(antonyms);
+
     searchedArea_contents.append(meanings);
   }
 }
